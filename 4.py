884777954aaca2cbe8a1fe76a9caadef66c91b8d
Juan Manuel Godoy <juanmanuelgodoy@yahoo.com.ar>
2024-09-04 04:56:39 -0300

    Configuración inicial

---
+++ 4.py
@@ -0,0 +1,8 @@
+""" 4. Una vez codificadas en Python las Clases de los puntos anteriores, 
+instancie los objetos tal como sucede en las siguientes instrucciones:
+mozo1 = Mozo(‘Alfredo’)
+mozo2 = Mozo(‘Alfredo’)
+Luego, responda lo siguiente:
+i. ¿Los identificadores mozo1 y mozo2 hacen referencia al mismo objeto?
+ii. ¿Son objetos equivalentes? Explique que significa que dos objetos lo sean.
+iii. ¿Los objetos ligados a mozo1 y mozo2 comparten la misma posición de memoria? """
---
+++ TesterPizzaeria.py
@@ -0,0 +1,8 @@
+class TesterPizzeria:
+    def main(self):
+# Solución de los puntos 5.a., 5.b, 5.c, ...
+
+
+        if __name__ == ‘__main__’:
+testerPizzeria = TesterPizzeria()
+testerPizzeria.main()
